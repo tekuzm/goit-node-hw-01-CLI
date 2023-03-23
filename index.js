6ec06fff6b6eff8e7dd5fab78ff9efe61478ee6a
TetianaKuzma <tetiana.b97@gmail.com>
2023-03-23 23:06:31 +0200

    Add functions and actions

---
+++ index.js
@@ -0,0 +1,43 @@
+const { program } = require("commander");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const argv = program.opts();
+
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const listContacts = await contacts.listContacts();
+      console.log(listContacts);
+      break;
+
+    case "get":
+      const getContact = await contacts.getContactById(id);
+      console.log(getContact);
+      break;
+
+    case "add":
+      const addContact = await contacts.addContact({ name, email, phone });
+      console.log(addContact);
+      break;
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
